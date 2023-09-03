acc505863fe555bf572128715e9ff828ed17bf65
Alexey Kirov <kirow.alesha@yandex.ru>
2023-09-03 11:53:38 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <time.h>
 using namespace std;
 
-int main() {
-    float x[1000] = {};
-    for(int i =0; i<1000; i++){
-        x[i] = 2 * ((rand()/(static_cast<double>(RAND_MAX))) - 0.5);
-        printf("x = %f", x[i]);
-    }
+int GetRandomNumber(int min, int max)
+{
+  int num = min + rand() % (max - min + 1);
+  return num;
 }
+
+int main() {
+    srand(time(NULL));
+    ofstream out("excel.csv");
+    double x[200] = {};
+    out << "\n";
+    for(int i =0; i<(sizeof(x)/sizeof(*x)); i++){
+        x[i] = 2 * ((rand()/(static_cast<double>(RAND_MAX))) - 0.5);
+        out << x[i] << "\n";
+        printf("%f\n", x[i]);
+    }
+    out.close();
